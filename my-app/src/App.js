34e120ed86452ed25c1b170ad9e0c71feca0a040
Brayan <bstmvel@gmail.com>
2024-05-26 00:19:25 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import LoginButton from './loguearse';
+import LogoutButton from './logout';
+import Profile from './perfil'
+
+
+function App() {
+  return (
+    <div className="App">
+      <LoginButton/>
+      <LogoutButton/>
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
